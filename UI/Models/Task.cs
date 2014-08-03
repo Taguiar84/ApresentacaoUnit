1a7d493b27794ccb736c95eff51c48b84fbdab13
Taguiar84 <thiago.sol@gmail.com>
2014-08-03 17:14:09 -0300

    projeto limpo

---
+++ UI/Models/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class Task
+    {
+        public string Nome { get; set; }
+
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+
+        public TaskStatus Status { get; set; }
+        public Responsavel Responsavel { get; set; }
+    }
+
+
+    //[Default]
+    public enum TaskStatus
+    {
+        Aberta,
+        Execucao,
+        Finalizada
+    }
+
+
+
+
+    public class Responsavel
+    {
+        public string Nome { get; set; }
+    }
+
+}

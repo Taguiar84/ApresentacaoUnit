97f65c1a42fbb19d9a8de7231272177d7aed75c0
Taguiar84 <thiago.sol@gmail.com>
2014-08-11 12:57:23 -0300

    fechando primeira versão

--- UI/Controllers/TaskController.cs
+++ UI/Controllers/TaskController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UI.Models;
 
 namespace UI.Controllers
 {
@@ -13,7 +14,85 @@ namespace UI.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.Task = Seed();
             return View();
+        }
+
+        public ActionResult Index2()
+        {
+            var model = Seed();
+            return View(model);
+        }
+
+
+        private Task[] Seed()
+        {
+            List<Responsavel> reposnsaveis = new List<Responsavel>();
+
+            reposnsaveis.Add(new Responsavel() { Nome = "Peter Jackson" });
+            reposnsaveis.Add(new Responsavel() { Nome = "Steve Jobs" });
+            reposnsaveis.Add(new Responsavel() { Nome = "Linux Trovalds" });
+
+            List<Task> itens = new List<Task>();
+
+            itens.Add(new Task()
+            {
+                Nome = "Fazer o Kernel"
+                ,
+                Prioridade = Prioridade.Alta
+                ,
+                Status = TaskStatus.Aberta
+                ,
+                Responsavel = reposnsaveis[2]
+            });
+
+            itens.Add(new Task()
+            {
+                Nome = "Roubar a interface do SO"
+                ,
+                Prioridade = Prioridade.PraOntem
+                ,
+                Status = TaskStatus.Finalizada
+                ,
+                Responsavel = reposnsaveis[1]
+            });
+
+            itens.Add(new Task()
+            {
+                Nome = "Forjar o Anel"
+                ,
+                Prioridade = Prioridade.PraOntem
+                ,
+                Status = TaskStatus.Execucao
+                ,
+                Responsavel = reposnsaveis[0]
+            });
+
+            itens.Add(new Task()
+            {
+                Nome = "Task 4"
+                ,
+                Prioridade = Prioridade.Baixa
+                ,
+                Status = TaskStatus.Execucao
+                ,
+                Responsavel = reposnsaveis[2]
+            });
+
+            itens.Add(new Task()
+            {
+                Nome = "Task 5"
+                ,
+                Prioridade = Prioridade.Normal
+                ,
+                Status = TaskStatus.Finalizada
+                ,
+                Responsavel = reposnsaveis[1]
+            });
+
+
+
+            return itens.ToArray();
         }
 
     }
